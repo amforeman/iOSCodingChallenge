4b5183b946a86adb195c53e2921d59da45f3538d
Annie Foreman <annie4man@gmail.com>
2024-02-16 10:06:26 -0600

    Initial Commit

---
+++ FetchCodingChallenge/FetchCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchCodingChallengeApp.swift
+//  FetchCodingChallenge
+//
+//  Created by Annie Foreman on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
